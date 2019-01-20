638d6814fa8b8fd431c6750a02bee09fffb5aecb
Evgeny Glukhov <evglukhov@luxoft.com>
2019-01-20 21:54:39 +0300

    fix grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { trigger, transition, keyframes, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('shockwaveJump', [
+      transition('* => play', [
+        animate('1s ease-out', keyframes([
+          style({ transform: 'scale(1)', offset: 0 }),
+          style({ transform: 'scale(1.08)', offset: 0.4 }),
+          style({ transform: 'scale(0.98)', offset: 0.5 }),
+          style({ transform: 'scale(1.02)', offset: 0.55 }),
+          style({ transform: 'scale(0.98)', offset: 0.6 }),
+          style({ transform: 'scale(1)', offset: 1 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'ngAnimate';
+
+  handleClick(btn: HTMLButtonElement) {
+    btn.dataset.animationState =
+      btn.dataset.animationState === 'play' ? 'stop' : 'play';
+  }
 }
